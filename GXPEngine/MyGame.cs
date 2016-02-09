3150b36f1f8b32a9c299513d22ed179504bdb4b6
mtesseracttech <mtesseracttech@gmail.com>
2016-02-09 11:38:40 +0100

    GXPEngine and GitIgnore

---
+++ GXPEngine/MyGame.cs
@@ -0,0 +1,17 @@
+using GXPEngine;
+
+public class MyGame : Game
+{
+    private static void Main()
+    {
+        new MyGame().Start();
+    }
+
+    public MyGame() : base(1280, 720, false, false)
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
