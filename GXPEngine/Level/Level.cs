d649da76164db5bf892675a2c9a286995b9a43dd
mtesseracttech <mtesseracttech@gmail.com>
2016-02-11 17:54:46 +0100

    Fixed(Maybe?) the color converter

--- GXPEngine/Level/Level.cs
+++ GXPEngine/Level/Level.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using GXPEngine.Utility.TiledParser;
 using NeonArkanoid.GXPEngine;
+using NeonArkanoid.GXPEngine.Managers;
+using NeonArkanoid.GXPEngine.Utils;
 using NeonArkanoid.Physics;
 using TiledParser;
 using System.Drawing.Drawing2D;
@@ -88,7 +91,12 @@ namespace NeonArkanoid.Level
                         {
                             if (property.Property.Name.ToLower() == "colour")
                             {
-                                polyColor = Convert.ToUInt32(property.Property.Value, 16) + 0xFF000000;
+                                List<char> chars = property.Property.Value.ToLower().ToCharArray().ToList();
+                                chars.Insert(2, 'f');
+                                chars.Insert(2, 'f');
+                                //string polyString = new string(chars.ToArray());
+                                polyColor = Convert.ToUInt32(new string(chars.ToArray()), 16);
+                                //polyColor = Convert.ToUInt32(property.Property.Value, 16) + 0xFF000000;
                             }
                         }
                         var poly = new Polygon(vectorArray, polyColor, this, tiledObject.X, tiledObject.Y);
@@ -98,6 +106,70 @@ namespace NeonArkanoid.Level
                 }
             }
         }
+        /*
+        void lineCollisionTest(LineSegment line)
+        {
+            int type = 0;
+
+            if (line.color == 0xffffff00)
+            {
+                type = 1;
+            }
+            else if (line.color == 0xffff0000)
+            {
+                type = 2;
+            }
+
+            Vec2 lineVector = line.end.Clone().Sub(line.start);
+            Vec2 lineVectorNormalized = lineVector.Clone().Normalize();
+            Vec2 lineNormal = lineVector.Clone().Normal();
+            float lineLength = lineVector.Length();
+            Vec2 ball2Line = _ball.position.Clone().Sub(line.start);
+
+            float ballDistance = ball2Line.Dot(lineNormal);
+            if (Math.Abs(ballDistance) < _ball.radius)
+            {
+                float ballDistanceAlongLine = ball2Line.Dot(lineVectorNormalized);
+                if (ballDistanceAlongLine < 0)
+                    ballDistanceAlongLine = 0;
+                if (ballDistanceAlongLine > lineLength)
+                    ballDistanceAlongLine = lineLength;
+                Vec2 closestPointOnLine = line.start.Clone().Add(lineVectorNormalized.Scale(ballDistanceAlongLine));
+                Vec2 difference = _ball.position.Clone().Sub(closestPointOnLine);
+                if (difference.Length() < _ball.radius)
+                {
+                    Vec2 normal = difference.Clone().Normalize();
+                    float separation = _ball.radius - difference.Length();
+                    _ball.position.Add(normal.Clone().Scale(separation));
+                    _ball.velocity.Reflect(normal, 0.5f);
+                    _ball.x = _ball.position.x;
+                    _ball.y = _ball.position.y;
+                    if (type == 2)
+                    {
+                        if (Math.Abs(_ball.velocity.Clone().Length()) < 3)
+                        {
+                            collisionCounter++;
+                            if (collisionCounter >= 50)
+                            {
+                                Respawn();
+                                _ball.position = mainTP.position.Clone();
+                                collisionCounter = 0;
+                            }
+                        }
+                    }
+                    if (Math.Abs(_ball.velocity.Clone().Length()) > 3)
+                    {
+                        SoundManager.PlaySound(SoundEffect.MSCOLLIDE, 1f, 0);
+                        if (line.color == 0xffff0000)
+                        {
+                            SoundManager.PlaySound(SoundEffect.BUMPER, 1, 0);
+                            Settings.score += 50 * multi;
+                        }
+                    }
+                }
+            }
+        }
+        */
 
         public string GetLevelName ()
         {
